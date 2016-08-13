cf36535c58f7b965ba2e359501fb43c5eb49771e
RumenTonev <rumen.tonev@yahoo.com>
2016-08-13 19:56:47 +0300

    Finished tutorial page

---
+++ KinderFirst/Controllers/PicturesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KinderFirst.Controllers
+{
+    public class PicturesController : Controller
+    {
+        // GET: Pictures
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SetSectionTutoPic(string id)
+        {
+            var model = new SectionDetails(id);
+            return View(model);
+        }
+
+   
+    }
+}
---
+++ KinderFirst/Controllers/SectionDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KinderFirst.Controllers
+{
+    public class SectionDetails
+    {
+        public string Id { get; set; }
+        public SectionDetails(string id)
+        {
+            this.Id = id;
+        }
+    }
+}
