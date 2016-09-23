dbbedda7f4d6ffcd5103ae0fec0baf742ed6a6a7
RumenTonev <rumen.tonev@yahoo.com>
2016-09-23 14:16:43 +0300

    Fix validation messages

--- KinderFirst/Models/GalleryItemView.cs
+++ KinderFirst/Models/GalleryItemView.cs
@@ -8,18 +8,19 @@ namespace KinderFirst.Models
 {
     public class GalleryItemView
     {
-        [MinLength(3),MaxLength(20)]
+
+        [MinLength(3, ErrorMessage = "Минимална дължина 3"), MaxLength(20, ErrorMessage = "Максимална дължина 20")]
         [Required(ErrorMessage = "Собственото име е задължително")]
         public string FirstName { get; set; }
-        [MinLength(3), MaxLength(20)]
+        [MinLength(3, ErrorMessage = "Минимална дължина 3"), MaxLength(20, ErrorMessage = "Максимална дължина 20")]
         [Required(ErrorMessage = "Фамилията е задължителна")]
         public string LastName { get; set; }
         [Required(ErrorMessage = "Имайлът е задължителен")]
         [EmailAddress(ErrorMessage = "Невалиден имейл адрес")]
-        [MinLength(3), MaxLength(20)]
+        [MinLength(3, ErrorMessage = "Минимална дължина 3"), MaxLength(20, ErrorMessage = "Максимална дължина 20")]
         public string Mail { get; set; }
         [Required(ErrorMessage = "Телефонният номер е задължителен")]
-        [RegularExpression("^([0-9]){4,10}$", ErrorMessage = "Моля въведет само цифри, от 4 до 10 символа дължина.")]
+        [RegularExpression("^([0-9]){4,10}$", ErrorMessage = "Моля въведете само цифри, от 4 до 10 символа дължина.")]
         public string Phone { get; set; }
         public string PicWidth { get; set; }
         public string PicHeight { get; set; }
